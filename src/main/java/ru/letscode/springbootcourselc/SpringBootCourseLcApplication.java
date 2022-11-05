e9f7bc5e0246c5c9d9733b6ca99b304ebdc00cef
Aleksey <lexasalnikov@gmail.com>
2022-11-05 11:47:52 +0500

    Init master

---
+++ src/main/java/ru/letscode/springbootcourselc/SpringBootCourseLcApplication.java
@@ -0,0 +1,13 @@
+package ru.letscode.springbootcourselc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCourseLcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCourseLcApplication.class, args);
+    }
+
+}
