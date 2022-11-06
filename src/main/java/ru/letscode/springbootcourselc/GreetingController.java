22c83c37bf5ab93bd2ab0b51ae628285d80953f6
Aleksey <lexasalnikov@gmail.com>
2022-11-06 16:08:20 +0500

    Lesson2

--- src/main/java/ru/letscode/springbootcourselc/GreetingController.java
+++ src/main/java/ru/letscode/springbootcourselc/GreetingController.java
@@ -1,24 +1,61 @@
 package ru.letscode.springbootcourselc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.letscode.springbootcourselc.domain.Message;
+import ru.letscode.springbootcourselc.repos.MessageRepo;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
-    @GetMapping("/greeting")
+
+    @Autowired
+    private MessageRepo messageRepo;
+
+    @GetMapping("/")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World")
                                String name, Map<String, Object> model){
         model.put("name", name);
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
-        model.put("some", "Hello, letsCode");
+        Iterable<Message> messageRepoAll = messageRepo.findAll();
+        model.put("messages ", messageRepoAll );
+        return "main";
+    }
+
+    @PostMapping("/main")
+    public String add(@RequestParam String text,
+                      @RequestParam String tag, Map<String, Object> model) {
+
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+
+        Iterable<Message> messageRepoAll = messageRepo.findAll();
+        model.put("messages", messageRepoAll );
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.put("messages", messages);
+
         return "main";
     }
 
---
+++ src/main/java/ru/letscode/springbootcourselc/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package ru.letscode.springbootcourselc.repos;
+
+import org.springframework.data.repository.CrudRepository;
+import ru.letscode.springbootcourselc.domain.Message;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}
